0d31e01014564d4bea6073ce7c78091921221c3a
刘猛 <759498475@qq.com>
2018-03-05 19:39:57 +0800

    增加博客内容

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,7 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-
+var webpack=require("webpack")
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -77,6 +77,13 @@ module.exports = {
       }
     ]
   },
+  plugins:[
+    new webpack.ProvidePlugin({
+      $:"jquery",
+      jQuery:"jquery",
+      "window.jQuery":"jquery"
+    })
+  ],
   node: {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
     // source contains it (although only uses it if it's native).
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,29 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div id="posts">
+    <div class="each-post">
+      <div class="title">
+        <a href="http://leanote.leanote.com/post/Leanote-Desktop-v2.6-released" title="全文">
+          Leanote Desktop v2.6 released 蚂蚁笔记桌面端2.6发布
+        </a>
+      </div>
+      <div class="created-time">
+        <i class="fa fa-clock-o"></i> 2017-12-01 12:08:14
+        &nbsp;&nbsp;
+        <span class="fa fa-eye"></span> 2739
+        &nbsp;&nbsp;
+        <span class="fa fa-thumbs-o-up"></span> 1
+        &nbsp;&nbsp;
+        <span class="fa fa-comments-o"></span> 13
+      </div>
+      <div class="desc">
+        <p>蚂蚁笔记桌面端2.6发布.</p><p>修复的问题:</p><ul><li>富文本编辑器查找替换高亮问题</li><li>启动有可能一直停留在Loading界面问题</li></ul><p>v2.4及以上版本可线上更新</p><p>下载地址:&nbsp;<a href="http://app.leanote.com/" data-mce-href="http://app.leanote.com">http://app.leanote.com</a></p>
+      </div>
+      <div id="moreContainer">
+        <a class="more" href="http://leanote.leanote.com/post/Leanote-Desktop-v2.6-released" title="全文">查看</a>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
@@ -14,21 +34,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
