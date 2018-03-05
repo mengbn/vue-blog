3aa87dcd317629e5c3ad6bedf5b03f072763a785
刘猛 <759498475@qq.com>
2018-03-05 14:52:06 +0800

    初始化博客前端

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Container>
+      <Aside width="200px">Aside</Aside>
+      <Main>haha</Main>
+    </Container>
+  </div>
+</template>
+
+<script>
+import { Container, Aside, Main } from 'element-ui'
+export default {
+  name: 'App',
+  components: {
+    Container,
+    Aside,
+    Main
+  }
+}
+</script>
+<style>
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+</style>
